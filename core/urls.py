bbd136134ffcb3ef330616f09b9de676843783fe
fiorellizz <fiorelligui@gmail.com>
2025-04-26 22:24:03 -0300

    feat: homepage

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.core, name="core"),
     path('daviejulia/', views.homepage, name="homepage"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 def homepage(request):
-
     return render(request, 'index.html')
+
+def core(request):
+    return render(request, 'core.html')
