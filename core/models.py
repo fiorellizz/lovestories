4e71d8c45b0ca229f9301b3d25cf035a79e62c4e
fiorellizz <fiorelligui@gmail.com>
2025-05-16 14:47:51 -0300

    fix: ajuste model

--- core/models.py
+++ core/models.py
@@ -23,14 +23,6 @@ class Musica(Base):
     def __str__(self):
         return f"{self.titulo} - {self.autor}"
 
-class Plano(Base):
-    slug  = models.SlugField(unique=True)      # 'basico', 'premium', etc.
-    nome  = models.CharField(max_length=50)     # 'Básico', 'Premium'
-    preco = models.DecimalField(max_digits=6, decimal_places=2)
-
-    def __str__(self):
-        return self.nome
-
 class Cliente(Base):
     nome_casal = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)  # ex: 'daviejulia'
@@ -41,7 +33,16 @@ class Cliente(Base):
     footer_title = models.CharField(max_length=100, default="Feliz 5 Meses Meu Amor")
     footer_subtitle = models.CharField(max_length=100, default="Que venham muitos mais meses, anos e décadas de amor, companheirismo e felicidade.")
     musicas = models.ManyToManyField(Musica, related_name='clientes')
-    plano = models.ForeignKey(Plano, on_delete=models.PROTECT, default='Basico')
+    PLANO_CHOICES = [
+        ('BASIC',   'Básico'),
+        ('PREMIUM', 'Premium'),
+    ]
+    plano = models.CharField(
+        max_length=10,
+        choices=PLANO_CHOICES,
+        default='BASIC',
+        verbose_name="Plano contratado"
+    )
 
     def __str__(self):
         return self.slug
